2f90812ed3cdf69ff271d3d8ca44608d170f7259
zostrow2001 <zostrow23@amherst.edu>
2021-10-15 10:31:46 -0400

    Added files

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,3 +1,22 @@
-## code to prepare `DATASET` dataset goes here
-
-usethis::use_data(DATASET, overwrite = TRUE)
+library(tidyverse)
+DiscGolf <-
+  readr::read_csv("pdga-approved-disc-golf-discs_2020-10-04T15-02-04.csv") %>%
+  janitor::clean_names() %>%
+  mutate(
+    model = iconv(disc_model, "latin1", "ASCII", sub = ""),
+    approved_date = lubridate::mdy(approved_date)
+  ) %>%
+  rename(
+    diameter = diameter_cm,
+    flexibility = flexibility_kg,
+    height = height_cm,
+    manufacturer = manufacturer_distributor,
+    rim_depth = rim_depth_cm,
+    rim_thickness = rim_thickness_cm,
+    weight = max_weight_gr
+  ) %>%
+  select(
+    approved_date, class, diameter, flexibility, height,
+    manufacturer, model, rim_depth, rim_thickness, weight
+  )
+usethis::use_data(DiscGolf)
